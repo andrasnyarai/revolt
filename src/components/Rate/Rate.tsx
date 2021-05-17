42514a95f817e1a5d65e2c759793923e0bfac4e5
rush <andrasnyarai@gmail.com>
2021-05-17 08:51:01 +0200

    add loading condition to Rate component

--- src/components/Rate/Rate.tsx
+++ src/components/Rate/Rate.tsx
@@ -9,11 +9,16 @@ type Props = {
 };
 
 export const Rate: React.VFC<Props> = ({ baseCurrency, targetCurrency }) => {
-  const rates = useStore((state) => state.rates);
+  const { rates, loading } = useStore((state) => state);
   const rate = rates[baseCurrency][targetCurrency];
 
   const baseAmount = 1;
   const targetAmount = roundToDecimals(baseAmount * rate, 6);
+
+  if (loading) {
+    return null;
+  }
+
   return (
     <div>
       {`${baseAmount} ${baseCurrency.toUpperCase()} = ${targetAmount} ${targetCurrency.toUpperCase()}`}
